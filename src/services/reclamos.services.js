cdefe71fc1144d7fc7767da6fbdf72e4ab86179a
Enrique Alejandro Pierotti Castillo <pierotticastillo@gmail.com>
2024-10-26 19:33:26 -0300

    Corrección par que no se envíe un correo si no se actualizo

--- src/services/reclamos.services.js
+++ src/services/reclamos.services.js
@@ -86,8 +86,8 @@ export const updateEmployee = async (idReclamo, estado, idUsuario) => {
             throw new Error("El reclamo no pertenece al empleado");
         }
         const updatedReclamo = await reclamosDataBase.updateEmployee(idReclamo, estado, idUsuario);
-        if (updatedReclamo.estadorReclamo === reclamoActual.estadoReclamo) {
-            console.error("No se encontró el reclamo o no se pudo actualizar.");
+        if (updatedReclamo[0].estadoReclamo === reclamoActual[0].estadoReclamo) {
+            console.error("No se pudo actualizar el reclamo");
             throw new Error("No se pudo actualizar el reclamo en la base de datos.");
         }
         const usuario = await usuariosService.getById(updatedReclamo[0].idUsuarioCreador)
